52cfd58e0aba186a0e9797ed59c5c43ea27ab00d
Anton <anton@happyorder.io>
2020-11-27 17:53:33 +0100

    now send to SQS, recevie from SQS, send email via SES, delete message in SQS

--- SQS-SES-Docker/src/index.js
+++ SQS-SES-Docker/src/index.js
@@ -6,19 +6,54 @@ const AWS = require('aws-sdk');
 const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 const queueURL = process.env.SQS_URL;
 
-const params = {
-  AttributeNames: [
-    'SentTimestamp',
-  ],
-  MaxNumberOfMessages: 1,
-  MessageAttributeNames: [
-    'All',
-  ],
-  QueueUrl: queueURL,
-  WaitTimeSeconds: 20,
-};
+async function sendEmail(email) {
+  // Create sendEmail params
+  const params = {
+    Destination: { /* required */
+      ToAddresses: [
+        email.to,
+      /* more items */
+      ],
+    },
+    Message: { /* required */
+      Body: { /* required */
+        Text: {
+          Charset: 'UTF-8',
+          Data: email.body,
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: email.subject,
+      },
+    },
+    Source: process.env.EMAIL, /* required */
+  };
+
+  const sendPromise = new AWS.SES({ apiVersion: '2010-12-01' }).sendEmail(params).promise();
+  sendPromise.then(
+    (data) => {
+      console.log(data.MessageId);
+    },
+  ).catch(
+    (err) => {
+      console.error(err, err.stack);
+    },
+  );
+}
 
 async function receiveEmailMessage() {
+  const params = {
+    AttributeNames: [
+      'SentTimestamp',
+    ],
+    MaxNumberOfMessages: 1,
+    MessageAttributeNames: [
+      'All',
+    ],
+    QueueUrl: queueURL,
+    WaitTimeSeconds: 20,
+  };
   console.log('start Receive');
   const MessageData = await sqs.receiveMessage(params).promise().catch((err) => {
     if (err.stack) {
@@ -26,27 +61,24 @@ async function receiveEmailMessage() {
       process.exit(1);
     }
   });
- 
+  if (MessageData.Messages !== undefined) {
     const EmailMessage = MessageData.Messages[0];
-    console.log(EmailMessage.MessageAttributes)
-  
+    const email = {
+      to: EmailMessage.MessageAttributes.To.StringValue,
+      subject: EmailMessage.MessageAttributes.Subject.StringValue,
+      body: EmailMessage.Body,
+    };
+    await sendEmail(email);
 
-  
-
-  receiveEmailMessage();
-  /*   const deleteParams = {
-    QueueUrl: queueURL,
-    ReceiptHandle: data.Messages[0].ReceiptHandle,
-  };
- */
-/*    sqs.deleteMessage(deleteParams, (err, data1) => {
-    if (err) {
+    const deleteParams = {
+      QueueUrl: queueURL,
+      ReceiptHandle: EmailMessage.ReceiptHandle,
+    };
+    sqs.deleteMessage(deleteParams).promise().catch((err) => {
       console.log('Delete Error', err);
-    } else {
-      console.log('Message Deleted', data1);
-    }
-  });
- */
+    });
+  }
+  receiveEmailMessage();
 }
-
+// sendEmail();
 receiveEmailMessage();
