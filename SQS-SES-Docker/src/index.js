dad4eed8e126e4c1b40a659a94a508eace7a621d
Anton <anton@happyorder.io>
2020-11-27 16:03:57 +0100

    Added SQS-SES-Docker

---
+++ SQS-SES-Docker/src/index.js
@@ -0,0 +1,49 @@
+const AWS = require('aws-sdk');
+const { exit } = require('process');
+// Set the region
+
+// Create an SQS service object
+
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+const queueURL = process.env.SQS_URL;
+
+const params = {
+  AttributeNames: [
+    'SentTimestamp',
+  ],
+  MaxNumberOfMessages: 1,
+  MessageAttributeNames: [
+    'All',
+  ],
+  QueueUrl: queueURL,
+  WaitTimeSeconds: 20,
+};
+
+async function receiveEmailMessage() {
+  console.log('start Receive');
+  const data = await sqs.receiveMessage(params).promise().catch((err) => {
+    if (err.stack) {
+      console.error('ReceiveMessage error', err);
+      process.exit(1);
+    }
+  });
+
+  console.log(data);
+
+  receiveEmailMessage();
+  /*   const deleteParams = {
+    QueueUrl: queueURL,
+    ReceiptHandle: data.Messages[0].ReceiptHandle,
+  };
+ */
+/*    sqs.deleteMessage(deleteParams, (err, data1) => {
+    if (err) {
+      console.log('Delete Error', err);
+    } else {
+      console.log('Message Deleted', data1);
+    }
+  });
+ */
+}
+
+receiveEmailMessage();
